bdbcd687be333b78f0e004a17aec0d5252fe55ce
Zh3nyaZh3nya <129783207+Zh3nyaZh3nya@users.noreply.github.com>
2023-04-06 23:42:44 +0600

    Добавил store vuex

--- src/main.js
+++ src/main.js
@@ -3,19 +3,16 @@ import 'material-design-icons-iconfont'
 
 import App from '@/App'
 import router from '@/router/router'
-import Vuex from 'vuex'
-import store from '@/vuex/store'
 import vue3StarRatings from "vue3-star-ratings";
-
+import store from './store/store'
 
 import { createApp } from 'vue'
 
 const app = createApp(App) 
 
-app.component("vue3-star-ratings", vue3StarRatings);
 
-app 
-    .use(Vuex)
+app
+    .component("vue3-star-ratings", vue3StarRatings) 
     .use(store)
     .use(router)
     .mount('#app')
